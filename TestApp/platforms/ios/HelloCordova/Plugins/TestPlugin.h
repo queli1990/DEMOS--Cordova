009bc42ea10ae8488bfac29c597dfc93481dafd6
queli <queli@gochinatv.com>
2016-11-18 11:50:27 +0800

    rename

--- TestApp/platforms/ios/HelloCordova/Plugins/TestPlugin.h
+++
@@ -1,9 +0,0 @@
-/********* Echo.h Cordova Plugin Header *******/
-
-#import <Cordova/CDV.h>
-
-@interface TestPlugin : CDVPlugin
-
-- (void)test:(CDVInvokedUrlCommand*)command;
-
-@end
---
+++ VegoTV--定制插件/platforms/ios/VegoTV/Plugins/TestPlugin.h
@@ -0,0 +1,15 @@
+//
+//  TestPlugin.h
+//  VegoTV
+//
+//  Created by mobile_007 on 16/11/17.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface TestPlugin : CDVPlugin
+
+- (void) test:(CDVInvokedUrlCommand *)command;
+
+@end
